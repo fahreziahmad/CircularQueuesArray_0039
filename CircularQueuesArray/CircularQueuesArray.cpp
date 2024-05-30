854ece48082095cb9f442836c050bae6dbb52d38
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-30 18:14:49 +0700

    add void remove cek apakah antrian kosongg

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -37,3 +37,11 @@ public:
 		queue_array[REAR] = num;
 	}
 };
+void remove() {
+	//cek
+	if (FRONT == -1) {
+		cout << "Queue underflow\n";
+		return;
+	}
+	cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+}
